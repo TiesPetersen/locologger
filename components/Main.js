a4ea65b5145777a4921f57728b6812c4b8a80144
Ties Petersen <tiesp2005@gmail.com>
2024-10-08 19:44:58 +0200

    padding fix

--- components/Main.js
+++ components/Main.js
@@ -54,7 +54,7 @@ export default function Main(props) {
     }
 
     return (
-        <main className='flex-1 flex flex-col p-4 sm:p-8'>
+        <main className='flex-1 flex flex-col px-4 pb-4'>
             {children}
         </main>
     )
